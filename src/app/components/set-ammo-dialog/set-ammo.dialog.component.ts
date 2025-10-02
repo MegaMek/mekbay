aec0fc5972f10ae3992c9c6eafc71b0878e6b8e1
Exeea <dev@exeea.com>
2025-10-03 00:01:42 +0200

    .

--- src/app/components/set-ammo-dialog/set-ammo.dialog.component.ts
+++ src/app/components/set-ammo-dialog/set-ammo.dialog.component.ts
@@ -110,7 +110,7 @@ export interface SetAmmoDialogData {
         }
 
         :host-context(.cdk-overlay-pane) {
-            transform: translateY(-10dvh);
+            transform: translateY(-10vh);
         }
 
         .content {
