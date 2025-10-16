0c6b57e8b8d7a1f81f9419be54e1eb89a0ec122c
Exeea <dev@exeea.com>
2025-10-16 16:54:34 +0200

    zindex

--- src/app/components/svg-viewer/svg-canvas-overlay.component.ts
+++ src/app/components/svg-viewer/svg-canvas-overlay.component.ts
@@ -136,7 +136,7 @@ interface brushLocation {
             position: absolute;
             top: 0; left: 0; right: 0; bottom: 0;
             pointer-events: none;
-            z-index: 5;
+            z-index: 1;
         }
         .drawing-canvas {
             pointer-events: none;
@@ -162,7 +162,7 @@ interface brushLocation {
             position: fixed;
             bottom: 0;
             right: 0;
-            z-index: 20;
+            z-index: 1;
             pointer-events: none;
         }
         .fab {
