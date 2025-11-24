f4c1ad71ac0fdce7da0b149a0e41eacfafe4e172
Exeea <dev@exeea.com>
2025-11-24 16:11:03 +0100

    removed unused import

--- src/app/components/unit-icon/unit-icon.component.ts
+++ src/app/components/unit-icon/unit-icon.component.ts
@@ -1,5 +1,4 @@
 import { Component, ChangeDetectionStrategy, inject, input, signal, effect, computed } from '@angular/core';
-import { toSignal } from '@angular/core/rxjs-interop';
 import { CommonModule } from '@angular/common';
 import { ImageStorageService } from '../../services/image-storage.service';
 import { Unit } from '../../models/units.model';
